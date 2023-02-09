5d7e8be2aac06f8c20371dea46e0a986f8662640
gyungwan <zddx1994@gmail.com>
2023-02-09 18:04:17 +0900

    인프런 알고리즘 2일차

--- Programmers/0.level/암호해독.js
+++ Programmers/0.level/암호해독.js
@@ -8,3 +8,14 @@ function solution(cipher, code) {
 
   return answer;
 }
+
+function solution(cipher, code) {
+  let answer = [];
+  let array = cipher.split("");
+  console.log(array);
+  for (let i = 0; i <= array.length; i++) {
+    console.log(array[code * i - 1]);
+    answer.push(array[code * i - 1]);
+  }
+  return answer.join("");
+}
