85e51660116c8196b6f3b1d6d4ac6849676cdc83
gyeongwan <113571059+gyungwan@users.noreply.github.com>
2023-08-01 11:20:54 +0900

    [unrated] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.py
@@ -1,9 +1,9 @@
 def solution(n):
     answer = 0
-    for i in range(n+1):
-        if  i % 2 == 0 and n % 2 == 0:
-            answer += i*i
-            print("i answer",i, answer)
-        elif i % 2 == 1 and n % 2 ==1:
-            answer += i    
+    if n % 2:
+        for i in range(1,n+1,2):
+            answer += i
+    else:
+        for i in range(2,n+1,2):
+            answer += i**2
     return answer
