6acc3d5962195d8a00e24b88446e38948d64bf27
gyeongwan <113571059+gyungwan@users.noreply.github.com>
2023-08-01 10:40:14 +0900

    [unrated] Title: 두 수의 연산값 비교하기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
+++ 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.py
@@ -1,10 +1,5 @@
 def solution(a, b):
-    str1 = str(a) + str(b)
-    num1 = 2 * a * b
-    if int(str1) > num1:
-        return int(str1)
-    elif int(str1) == num1:
-        return int(str1)
-    else:
-        return num1  
+        return max(int(str(a) + str(b)), 2 * a * b)
+
+        
     
